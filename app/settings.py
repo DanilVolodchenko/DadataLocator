ca52ec8c44b791cf21111e2bd5078c30dceca54f
Danil Volodchenko <danil.volodchenko@inbox.ru>
2023-11-20 10:08:48 +0700

    Deleting unused constant

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,4 @@
 DEFAULT_URL_ADDRESS = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address'
 DEFAULT_LANGUAGE = 'ru'
 DEFAULT_DB_FILE = '../db_settings.sqlite3'
-API_KEY_LENGTH = 40
 NUMBER_OF_RESULT = 10
